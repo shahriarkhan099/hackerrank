bacda5d3f91107cff5741e6207758a160f482bc3
Md Shahriar Khan <77568415+ShahriarKhan099@users.noreply.github.com>
2022-04-07 17:19:38 +0600

    Add files via upload

---
+++ JavaLoopsII.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tests = sc.nextInt();
+        long a, b, c, temp, temp2;
+        for(long i = 0; i<tests; i++) {
+            a = sc.nextLong();
+            b = sc.nextLong();
+            c = sc.nextLong();
+            temp = a;
+
+            for (long j = 0; j<c; j++) {
+                if(j==c-1) {
+                    temp+=Math.pow(2, j)*b;
+                    System.out.println(temp);
+                }
+                else {
+                    temp+=Math.pow(2, j)*b;
+                    System.out.print(temp+" ");
+                }
+            }
+        }
+    }
+}
