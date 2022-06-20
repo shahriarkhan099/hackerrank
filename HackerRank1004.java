30f3dec8e353a3a31552b07f56dea943b7c9866e
Md Shahriar Khan <77568415+shahriarkhan099@users.noreply.github.com>
2022-06-20 23:04:49 +0600

    Add files via upload

---
+++ HackerRank1004.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class HackerRank1004 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i = sc.nextInt();
+        double d = sc.nextDouble();
+        sc.nextLine();
+        String s = sc.nextLine();
+
+        System.out.println("String: " + s + "\nDouble: " + d + "\nInt: " + i);
+    }
+}
