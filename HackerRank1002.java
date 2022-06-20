9943dedeb3c10ce5a0cf808a6865554a1e5f9efe
Md Shahriar Khan <77568415+shahriarkhan099@users.noreply.github.com>
2022-06-20 22:24:11 +0600

    Add files via upload

---
+++ HackerRank1002.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class HackerRank1002 {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        ArrayList<Integer> inputArray = new ArrayList<Integer>();
+        takeInput(inputArray);
+        inputArray.forEach(n-> System.out.println(n));
+    }
+    public static void takeInput(ArrayList<Integer> arr) {
+       while(sc.hasNextInt()) {
+           arr.add(sc.nextInt());
+       }
+    }
+}
