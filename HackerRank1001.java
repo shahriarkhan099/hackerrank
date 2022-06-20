5b0090c84f9050319b0ed2afcf7400e0f00d168d
Md Shahriar Khan <77568415+shahriarkhan099@users.noreply.github.com>
2022-06-20 21:53:00 +0600

    Add files via upload

---
+++ HackerRank1001.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class HackerRank1001 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
